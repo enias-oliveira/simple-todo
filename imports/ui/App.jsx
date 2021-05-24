a02a2ef969874eac0f10f7b051dd71158093a935
Enias Oliveira <eniasoliveira27@gmail.com>
2021-05-24 14:23:30 -0300

    feat: adds task collection mongo setup

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from 'react'
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+import { useTracker } from 'meteor/react-meteor-data'
+
+import { TasksCollection } from '../api/collections/tasks-collection.js'
+import Task from './components/Task.jsx'
+
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch())
+  return (
+    <div>
+      <h1>Hello Meteor</h1>
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ))}
+      </ul>
+    </div>
+  )
+}
