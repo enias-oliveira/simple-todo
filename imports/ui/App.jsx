76dfca2b90513e25da23121c789d2380c9b65c7c
Enias Oliveira <eniasoliveira27@gmail.com>
2021-05-24 15:23:09 -0300

    feat: adds task completion or deletion

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,17 +7,32 @@ import { TasksCollection } from '/imports/api/collections/tasks-collection.js'
 import Task from './components/Task'
 import TaskForm from './components/TaskForm'
 
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked,
+    },
+  })
+}
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id)
+
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch())
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch())
   return (
     <div>
       <h1>Hello Meteor</h1>
+      <TaskForm />
       <ul>
         {tasks.map((task) => (
-          <Task key={task._id} task={task} />
+          <Task
+            key={task._id}
+            task={task}
+            onCheckboxClick={toggleChecked}
+            onDeleteClick={deleteTask}
+          />
         ))}
       </ul>
-      <TaskForm />
     </div>
   )
 }
--- imports/ui/components/Task/index.jsx
+++ imports/ui/components/Task/index.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Task = ({ task }) => {
-  return <li>{task.text}</li>
+const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={!!task.isChecked}
+        onClick={() => onCheckboxClick(task)}
+        readOnly
+      />
+      <span>{task.text}</span>
+      <button onClick={() => onDeleteClick(task)}>&times;</button>
+    </li>
+  )
 }
 
 Task.propTypes = {
   task: PropTypes.shape({
     _id: PropTypes.number,
     text: PropTypes.string,
+    isChecked: PropTypes.bool,
   }),
+  onCheckboxClick: PropTypes.func,
 }
 
 export default Task
